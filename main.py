250165842ff72d00168b1cdb906fc1b1f58ca619
nick747 <84738664+nick747@users.noreply.github.com>
2023-06-21 12:45:46 +0200

    Added selectionSort

--- main.py
+++ main.py
@@ -23,3 +23,15 @@ def bubbleSort(array):
                 array[j], array[j + 1] = array[j + 1], array[j]
 
     print("Sorted with Bubble Sort: ", array)
+
+# Selection Sort
+
+
+def selectionSort(array):
+    n = len(array)
+    for i in range(n):
+        min_i = i
+        for j in range(i + 1, n):
+            if array[j] < array[min_i]:
+                min_i = j
+        array[i], array[min_i] = array[min_i], array[i]
