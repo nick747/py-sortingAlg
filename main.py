983e8fc1cba8776eaf2a97f0d9c64771fe42396b
nick747 <84738664+nick747@users.noreply.github.com>
2023-06-21 12:47:33 +0200

    Added Merge Sort

--- main.py
+++ main.py
@@ -44,3 +44,29 @@ def insertionSort(array):
             array[j + 1] = array[j]
             j -= 1
         array[j + 1] = key
+
+# Merge Sort
+
+
+def mergeSort(array):
+    if len(array) <= 1:
+        return array
+    mid = len(array) // 2
+    left = mergeSort(array[:mid])
+    right = mergeSort(array[mid:])
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
