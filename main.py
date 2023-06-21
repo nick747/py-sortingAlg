0e797ae51fe0589a9556eec15d6d02af80c40ebb
nick747 <84738664+nick747@users.noreply.github.com>
2023-06-21 12:48:11 +0200

    Added QuickSort

--- main.py
+++ main.py
@@ -70,3 +70,14 @@ def merge(left, right):
     result.extend(left[i:])
     result.extend(right[j:])
     return result
+
+
+# Quick Sort
+def quickSort(array):
+    if len(array) <= 1:
+        return array
+    pivot = array[len(array) // 2]
+    left = [x for x in array if x < pivot]
+    middle = [x for x in array if x == pivot]
+    right = [x for x in array if x > pivot]
+    return quickSort(left) + middle + quickSort(right)
